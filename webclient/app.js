b70ad7b6fc866d34dc19f2aa3885e16d9875a160
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2021-04-20 19:25:01 +0200

    client minor fixes

--- webclient/app.js
+++ webclient/app.js
@@ -1,59 +1,35 @@
 const app = angular.module("rkchat", []);
 
+function capFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
 app.controller("rkchat_controller", $scope => {
     $scope.userOnline = false;
-    $scope.username = "fds";
+    $scope.username = null;
     $scope.currentGroup = 'public';
     $scope.groups = {
-        'public': [
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Kekec', 'timestamp': '20:45', 'message': 'kekec test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' }
-        ],
-        'janez': [
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' }
-        ],
-        'toni': [
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': 'Janez', 'timestamp': '20:45', 'message': 'test test' },
-            { 'sender': '_', 'timestamp': '20:45', 'message': 'test test' }
-        ]
+        'public': []
     }
 
     $scope.enterChat = () => {
+        $scope.username = capFirstLetter(document.getElementById('username').value);
         $scope.userOnline = true;
         Swal.fire({
             position: 'top-end',
             icon: 'success',
             title: `You have just logged in as ${$scope.username}`,
             showConfirmButton: false,
-            timer: 1500
         });
         // @TODO connect to socket
     }
 
     $scope.addGroup = () => {
         $scope.groups["nekrandom"] = {};
-        console.log("okej");
-        Swal.fire('Oops...', 'Something went wrong!', 'error')
     }
 
     $scope.changeGroup = groupName => {
-        alert("keyyy");
-        $scope.groupName = groupName;
+        $scope.currentGroup = groupName;
         $scope.$apply();
     }
 
