f899f2e7f11880cae9a267c2d24f05de257f5dbe
JanezSedeljsak <janez.sedeljsak@gmail.com>
2021-04-27 01:02:54 +0200

    utf-8 encoding tmp fix

--- uiclient/js/services.js
+++ uiclient/js/services.js
@@ -43,6 +43,9 @@ app.service('$parser', function () {
 
     this.sendData = (socketClient, object) => {
         const jsonString = JSON.stringify(object);
+        // tmp fix remove non utf-8 characters
+        jsonString = jsonString.replace(/[^\x20-\x7E]/g, '');
+
         const byteHeader = pystruct.pack("!H", jsonString.length);
         socketClient.write(byteHeader + jsonString);
     };
